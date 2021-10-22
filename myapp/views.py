ed35a0bb7f6c585c6d049637b06e3035acb4f320
Rubyroy12 <ibrakipz7@gmail.com>
2021-10-22 22:39:01 +0300

    consume covid19 api

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def index(request):
+    response= requests.get('https://api.covid19api.com/countries').json()
+
+    params = {
+        'response':response
+    }
+    return render(request, 'index.html',params)
+# def data(request):
+   
+
+#     return render(request, 'index.html',params)
